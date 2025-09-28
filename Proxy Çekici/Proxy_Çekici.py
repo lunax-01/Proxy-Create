51a70dd9250429375d7b93b2f4709841c2e0fbd3
LuNaX <hayaletdemir0@gmail.com>
2025-09-28 16:43:28 +0300

    Proxy Çekici
    Sorumluluk Şahısa Aittir Discord:https://discord.gg/rj5PvyMAkW

---
+++ Proxy Çekici/Proxy_Çekici.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import re
+import time
+import random
+import requests as ModcaTheLost
+
+class modca:
+	def __init__(self):
+		url = "https://free-proxy-list.net/"
+		proxies = []
+		proxy1 = ModcaTheLost.get(url).text
+		proxy2 = re.findall(r"<tr><td>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})</td><td>(\d+?)</td>", proxy1)
+		for x in proxy2:
+			proxies.append(":".join(x))
+		try:tanya_total = int(input("Kaçtane Çekilsin -> "))
+		except:tanya_total = 1
+		print("Az Dur :)")
+		time.sleep(3) 
+		for total in range(tanya_total):
+			total +=1
+			proxy = random.choice(proxies)
+			print("proxy %s -> %s"%(total, proxy))
+		ask = input("Devam etmek istiyormusun? Y/N ")
+		if ask in ["Y", "y"]:
+			modca()
+		else:
+			exit("Bay Kankam =) ")
+try:
+	modca()
+except Exception as e:
+	exit(str(e))
+	
+	#@LunaxKurucu
---
+++ README.txt
@@ -0,0 +1,3 @@
+MADE IN LUNAX Discord 
+Server Link:https://discord.gg/rj5PvyMAkW
+Rastgele Proxy Oluşturur Sınır Yoktur Sınırsızdır Ücretsizdir :)
